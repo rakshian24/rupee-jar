514a7dc4ee35686dc0d42ff8b3d39f4513416f5c
rakshian24 <rakshithrao123@gmail.com>
2025-07-03 23:26:44 +0530

    feat: added graphql typedefs

--- backend/graphql/resolvers/index.ts
+++ backend/graphql/resolvers/index.ts
@@ -1,13 +1,10 @@
 import userResolvers from "./users";
-import todoResolvers from "./todo";
 
 export default {
   Query: {
     ...userResolvers.Query,
-    ...todoResolvers.Query,
   },
   Mutation: {
     ...userResolvers.Mutation,
-    ...todoResolvers.Mutation,
   },
 };
